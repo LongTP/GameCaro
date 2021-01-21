329ebdce97eb3fa5bfe929b26abb2ea5c52dce05
Long Phan Thanh <longfanna@gmail.com>
2021-01-21 21:23:12 +0700

    Add project files.

---
+++ About.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GameCaro
+{
+    partial class frmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.ForeColor = System.Drawing.Color.Black;
+            this.lblAbout.Location = new System.Drawing.Point(109, 9);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(130, 20);
+            this.lblAbout.TabIndex = 0;
+            this.lblAbout.Text = "Đồ án môn học";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(99, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Trò chơi cờ Caro";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(52, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 164);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thành viên nhóm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(7, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(214, 136);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Smt in here i hope";
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 268);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAbout);
+            this.Name = "frmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class frmCaroChess : Form
+    {
+
+        public static int cdStep = 100;
+        public static int cdTime = 15000;
+        public static int cdInterval = 100;
+        private CaroChess caroChess;
+        private Graphics grs;
+        
+        public frmCaroChess()
+        {
+            InitializeComponent();
+            caroChess = new CaroChess();
+            caroChess.CreateChessPieces();
+            grs = pnlChessBoard.CreateGraphics();
+            PvC.Click += PvC_Click;
+            btnComputer.Click += PvC_Click;
+            exitToolStripMenuItem.Click += btnExit_Click;
+            prcbCoolDown.Step = cdStep;
+            prcbCoolDown.Maximum = cdTime;
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Interval = cdInterval;
+
+
+        }
+
+        private void PvC_Click(object sender, EventArgs e)
+        {
+            grs.Clear(pnlChessBoard.BackColor);
+            caroChess.StartPvC(grs);
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Start();
+        }
+
+        private void pnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            caroChess.DrawChessBoard(grs);
+            caroChess.RepaintChess(grs);
+        }
+
+        private void frmCaroChess_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(58, 58, 60);
+            btnComputer.BackColor = this.BackColor;
+            btnNewGame.BackColor = this.BackColor;
+            
+
+            btn2Player.BackColor = this.BackColor;
+            btnExit.BackColor = this.BackColor;
+            pnlChessBoard.BackColor = Color.White;
+
+        }
+
+        private void pnlChessBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!caroChess.Ready)
+                return;
+            if (caroChess.PlayChess(e.X, e.Y, grs))
+            {
+                if (caroChess.Mode == 1)
+                {
+                    if (caroChess.CheckWin())
+                    {
+                        tmCoolDown.Stop();
+                        caroChess.EndGame();
+                        return;
+                    }
+                }
+                else if (caroChess.Mode == 2)
+                {
+                    caroChess.LaunchComputer(grs);
+                    if (caroChess.CheckWin())
+                    {
+                        tmCoolDown.Stop();
+                        caroChess.EndGame();
+                        return;
+                    }
+                }
+              
+            }
+            tmCoolDown.Start();
+            prcbCoolDown.Value = 0;
+        }
+
+        public void OtherPlayerMark(Point point)
+        {
+            if (!caroChess.Ready)
+                return;
+            if (caroChess.PlayChess(point.X, point.Y, grs))
+            {
+                pnlChessBoard.Enabled = true;
+                if (caroChess.CheckWin())
+                {
+                    tmCoolDown.Stop();
+                    caroChess.EndGame();
+                }
+            }
+        }
+        private void PvsP(object sender, EventArgs e)
+        {
+            grs.Clear(pnlChessBoard.BackColor);
+            caroChess.StartPvP(grs);
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Start();
+
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            caroChess.Undo(grs);
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            caroChess.Redo(grs);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Do you want to exit!", "Exit", MessageBoxButtons.YesNo); ;
+
+            if (dlr == DialogResult.Yes)
+            {        
+                Application.Exit();
+            }
+
+
+        }
+        private void NewGame()
+        {
+            grs.Clear(pnlChessBoard.BackColor);
+            
+            tmCoolDown.Start();
+            prcbCoolDown.Value = 0;
+        }
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+
+            if (caroChess.Mode == 0)
+            {
+                MessageBox.Show("Chưa chọn chế độ chơi!", "Thông báo");
+            }
+            else if (caroChess.Mode == 1)
+            {
+                grs.Clear(pnlChessBoard.BackColor);
+                caroChess.StartPvP(grs);
+            }
+            else if(caroChess.Mode == 2)
+            {
+                grs.Clear(pnlChessBoard.BackColor);
+                caroChess.StartPvC(grs);
+            }
+            tmCoolDown.Start();
+            prcbCoolDown.Value = 0;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAbout frm = new frmAbout();
+            frm.Show();
+        }
+
+       
+
+        private void frmCaroChess_Shown(object sender, EventArgs e)
+        {
+            
+        }
+        void Listen()
+        {
+
+           
+        }
+
+        
+
+        private void tmCoolDown_Tick(object sender, EventArgs e)
+        {
+            prcbCoolDown.PerformStep();
+
+            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
+            {
+                tmCoolDown.Stop();
+                caroChess.EndGame();
+
+            }
+        }
+
+    
+        
+    }
+}
